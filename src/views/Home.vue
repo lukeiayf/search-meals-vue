069cb9d8e2c31ea403cccc7cc0c3190aa8fe3301
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-07-08 14:58:41 -0300

    adds home and store

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex flex-col items-center p-8 justify-center">
+        <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for meals" />
+        <div class="flex justify-center gap-2 mt-2">
+            <RouterLink :to="{ name: 'byLetter', params: { letter } }" v-for="letter of letters.split('')"
+                :key="letter">
+                {{ letter }}
+            </RouterLink>
+        </div>
+    </div>
+
+</template>
+<script setup>
+import { computed } from 'vue';
+import store from '../store';
+import { RouterLink } from 'vue-router';
+
+const meals = computed(() => store.state.meals);
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+</script>
